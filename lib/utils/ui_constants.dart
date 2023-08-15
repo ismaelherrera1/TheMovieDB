46f1aa017d6a88ae0d83d9971586292944f43b13
Ismael Herrera <tismaelherrera@gmail.com>
2023-08-15 13:26:48 -0300

    module 2 accomplished

---
+++ lib/utils/ui_constants.dart
@@ -0,0 +1,10 @@
+abstract class UIConstants {
+  static const String movieAppTitle = 'Movies App';
+  static const double genresFontSize = 12;
+  static const double ratingStarPoints = 5;
+  static const double titleFontsize = 28;
+  static const double subtitleFontSize = 20;
+  static const double likeFontSize = 12;
+  static const double appBarRadiusBorder = 25;
+  static const double defaultPadding = 8.0;
+}
---
+++ lib/widgets/movie_actions.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import '../utils/ui_constants.dart';
+
+class MovieActions extends StatefulWidget {
+  final double voteAverage;
+  int voteCount;
+  MovieActions(
+    this.voteAverage,
+    this.voteCount, {
+    super.key,
+  });
+
+  @override
+  State<MovieActions> createState() => _MovieActionsState();
+}
+
+class _MovieActionsState extends State<MovieActions> {
+  @override
+  Widget build(BuildContext context) {
+    double fontSize = 20;
+    var movieRatingText = '${widget.voteAverage}';
+    var likeCounterText = '${widget.voteCount} likes';
+    if (widget.voteCount > 999) {
+      likeCounterText = '+999 likes';
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ButtonBar(
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {},
+              child: const Icon(
+                Icons.play_circle,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Icon(
+                Icons.save_alt,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  widget.voteCount++;
+                });
+              },
+              child: const Icon(
+                Icons.thumb_up,
+              ),
+            ),
+            Text(
+              likeCounterText,
+              style: const TextStyle(
+                fontSize: UIConstants.likeFontSize,
+              ),
+            ),
+          ],
+        ),
+        Container(
+          decoration: const ShapeDecoration(
+            color: Colors.redAccent,
+            shape: StarBorder(
+              points: UIConstants.ratingStarPoints,
+            ),
+          ),
+          child: Text(
+            movieRatingText,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: fontSize,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
